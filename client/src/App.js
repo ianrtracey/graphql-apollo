0b27089839fb8cd8d79cc27984dcfc2b67727fdc
Ian Tracey <ian.tracey@latitudeengineering.com>
2017-10-26 00:16:45 -0700

    Add mock interface

--- client/src/App.js
+++ client/src/App.js
@@ -2,39 +2,52 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {
-  gql,
   graphql,
   ApolloProvider,
 } from 'react-apollo';
+import ApolloClient from 'apollo-client-preset';
+import gql from 'graphql-tag';
 import {
-  ApolloClient,
-} from 'apollo-client-preset';
+  makeExecutableSchema,
+  addMockFunctionsToSchema
+} from 'graphql-tools';
+import { mockNetworkInterfaceWithSchema } from 'apollo-test-utils';
+import { typeDefs } from './schema';
+const schema = makeExecutableSchema({ typeDefs });
+addMockFunctionsToSchema({ schema });
 
-const client = new ApolloClient();
+const mockNetworkInterface = mockNetworkInterfaceWithSchema({ schema });
+
+
+
+const client = new ApolloClient({
+  networkInterface: mockNetworkInterface,
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          <ChannelsList />
-        </p>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            <ChannelsListWithData />
+          </p>
+        </div>
+      </ApolloProvider>
     );
   }
 }
-
 const channelsListQuery = gql`
-    query ChannelsListQuery {
-      channels {
-        id
-        name
-      }
-    }
+query ChannelsListQuery {
+  channels {
+    id
+    name
+  }
+}
 `
 const ChannelsList = ({ data: { loading, error, channels }}) => {
   if (loading) {
