0ceec6eb6dbd97271c83895f56cfda9dbe509d5b
itracey <itracey@atlassian.com>
2017-10-26 22:28:43 -0700

    refresh

--- client/src/schema.js
+++ client/src/schema.js
@@ -1,12 +1,11 @@
 export const typeDefs = `
 
 type Channel {
-    id: ID!
-    name: String
+  id: ID!
+  name: String
 }
 
-# This type specifies entry point in our API
 type Query {
-    channels: [Channel]
+  channels: [Channel]
 }
 `;
